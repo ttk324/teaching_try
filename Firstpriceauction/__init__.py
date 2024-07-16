517ccb18bfc6804dd628ab123f18d187ed7ef6bc
Tiffany TSE <60918278+ttk324@users.noreply.github.com>
2024-07-16 16:06:56 +0900

    Add files via upload

---
+++ Firstpriceauction/__init__.py
@@ -0,0 +1,75 @@
+
+from otree.api import *
+c = cu
+
+doc = '\nIn a common value auction game, players simultaneously bid on the item being\nauctioned.<br/>\nPrior to bidding, they are given an estimate of the actual value of the item.\nThis actual value is revealed after the bidding.<br/>\nBids are private. The player with the highest bid wins the auction, but\npayoff depends on the bid amount and the actual value.<br/>\n'
+class C(BaseConstants):
+    NAME_IN_URL = 'Firstpriceauction'
+    PLAYERS_PER_GROUP = 3
+    NUM_ROUNDS = 1
+    EVALUATE_MIN = cu(0)
+    EVALUATE_MAX = cu(100)
+    INSTRUCTIONS_TEMPLATE = 'Firstpriceauction/instructions.html'
+class Subsession(BaseSubsession):
+    pass
+def creating_session(subsession: Subsession):
+    session = subsession.session
+    import random 
+    subsession.group_randomly()
+    for player in subsession.get_players():
+        value=random.uniform(C.EVALUATE_MIN,C.EVALUATE_MAX)
+        player.item_value=round(value, 2)
+    
+    for p in subsession.get_players():
+        if p.id_in_group==1:
+            p.role_type="seller"
+        if p.id_in_group==2:
+            p.role_type="buyer"
+        if p.id_in_group==3:
+            p.role_type="buyer"
+    
+class Group(BaseGroup):
+    highest_bid = models.CurrencyField()
+def set_winner(group: Group):
+    import random
+    
+    players = group.get_players()
+    group.highest_bid = max([p.bid_amount for p in players])
+    players_with_highest_bid = [p for p in players if p.bid_amount == group.highest_bid]
+    
+    winner = random.choice(players_with_highest_bid)  
+    # if tie, winner is chosen at random
+    winner.is_winner = True
+    
+    for p in players:
+        set_payoff(p)
+class Player(BasePlayer):
+    item_value = models.CurrencyField(doc='Estimate of the common value may be different for each player')
+    bid_amount = models.CurrencyField(doc='Amount bidded by the player', label='Bid amount', max=100, min=0)
+    is_winner = models.BooleanField(doc='Indicates whether the player is the winner', initial=False)
+    role_type = models.StringField()
+def set_payoff(player: Player):
+    group = player.group
+    if player.role_type=="buyer":
+        if player.is_winner:
+            player.payoff = player.item_value - player.bid_amount
+        #if player.payoff < 0:
+         #   player.payoff = 0
+        else:
+            player.payoff = 0
+    
+    if player.role_type=="seller":
+        player.payoff=group.highest_bid
+class Introduction(Page):
+    form_model = 'player'
+class Bid(Page):
+    form_model = 'player'
+    form_fields = ['bid_amount']
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.role_type=="buyer"
+class ResultsWaitPage(WaitPage):
+    after_all_players_arrive = set_winner
+class Results(Page):
+    form_model = 'player'
+page_sequence = [Introduction, Bid, ResultsWaitPage, Results]
---
+++ settings.py
@@ -0,0 +1,21 @@
+from os import environ
+SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=1, participation_fee=0)
+SESSION_CONFIGS = [dict(name='firstpriceauction', num_demo_participants=None, app_sequence=['Firstpriceauction']), dict(name='secondpriceauction', num_demo_participants=None, app_sequence=['Secondpriceauction']), dict(name='nodisclosure', num_demo_participants=None, app_sequence=['nodisclosure'])]
+LANGUAGE_CODE = 'en'
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True
+DEMO_PAGE_INTRO_HTML = ''
+PARTICIPANT_FIELDS = []
+SESSION_FIELDS = []
+ROOMS = []
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+SECRET_KEY = 'blahblah'
+
+# if an app is included in SESSION_CONFIGS, you don't need to list it here
+INSTALLED_APPS = ['otree']
+
+
